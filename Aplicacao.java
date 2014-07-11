1814deb8180b9fc9cfcf1fe62e9c5c3d6e2827fe
Rodrigo Cabral <rodrigo.silva@dataprev.gov.br>
2014-07-11 15:08:13 -0300

    Fixando uso de variaveis começadas por _ e $

--- Aplicacao.java
+++ Aplicacao.java
@@ -2,6 +2,7 @@ import br.rodrigo.Dispositivo;
 import br.rodrigo.processos.Produtor;
 import br.rodrigo.estudoHeranca.CarroCorrida;
 import br.rodrigo.estudoHeranca.CarroCorridaEspecializado;
+import br.rodrigo.variaveis.Variaveis;
 
 public class Aplicacao{
 
@@ -19,9 +20,10 @@ public class Aplicacao{
   produtor.gerarResultados();
     System.out.println("Teste de Processo"); */
 
+	Variaveis v = new Variaveis();
+	System.out.println("Quantidade " + v.get_Quantidade());
+	System.out.println("valor " + v.get$Valor());
 	
-	CarroCorridaEspecializado c = new CarroCorridaEspecializado();	
-	System.out.println("Velocidade: " + c.getVelocidade());
-  }
 
+}
 }
---
+++ br/rodrigo/variaveis/Variaveis.java
@@ -0,0 +1,39 @@
+package br.rodrigo.variaveis;
+
+/** Estudo de declaração de variáveis. Objetivo de fixar
+a informação de que a variável pode começar com _ e $*/
+public class Variaveis{
+
+public Variaveis(){
+ this.$valor = 12.01f;
+}
+
+// Copiou idéia do PHP? :)
+private float $valor;
+
+private int _quantidade;
+
+public float $altura;
+public int _chamadas;
+public String  _valores;
+
+
+
+
+
+public float get$Valor	(){
+	return this.$valor;
+}
+public void set$Valor(float $valor){
+	this.set$Valor(this.$valor = $valor);
+}
+
+public int get_Quantidade(){
+	return this._quantidade;
+}
+public void set_Quantidade(int _quantidade){
+	this._quantidade = _quantidade;
+}
+
+
+}
