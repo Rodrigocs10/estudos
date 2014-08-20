0755b2cabc507e415b11b6917c43867cf52fc079
Rodrigo Cabral <rodrigo.silva@dataprev.gov.br>
2014-08-20 15:59:57 -0300

    Exemplo de funcionamento de variável de instância

---
+++ br/rodrigo/variaveis_instancia/Frog.java
@@ -0,0 +1,17 @@
+package br.rodrigo.variaveis_instancia;
+
+public class Frog{
+	static int count = 0;
+	public Frog(){
+		count++;
+	}
+	
+	public static void main(String[] args){
+		new Frog();
+		new Frog();
+		new Frog();
+
+		System.out.println("Count " + count);
+	}
+
+}
